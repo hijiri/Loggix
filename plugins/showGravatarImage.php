c0c2a8d4fa3e9bed1af12c35ff4bb211c2691526
hijiri <web_company@hotmail.com>
2010-05-21 22:22:35 +0900

    The HTML and CSS is adjusted for plugin "showGravatarImage.php".

--- plugins/showGravatarImage.php
+++ plugins/showGravatarImage.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Loggix_Plugin - Show Gravatar Image alpha
+ * Loggix_Plugin - Show Gravatar Image
  *
  * @copyright Copyright (C) UP!
  * @author    hijiri
@@ -10,67 +10,65 @@
  * @version   10.5.21
  */
 
-$this->plugin->addFilter('recent-comments-text', 'exampleFunction', 1);
-
-function exampleFunction($text)
-{
-//    $pattern     = '<a href="([\.?\./]+)index.php\?id=([0-9]{1,})#c([0-9]{1,})" class="(.+)" title="(.+)">';
-//    $replacement = '<a href="\1/index.php?id=\2#c\3" class="\4" title="\5">';
-//    $text = mb_ereg_replace($pattern, $replacement, $text, 'l');
-
-    $pattern     = '/<a href="([\.?\.\/]+)index.php\?id=([0-9]{1,})#c([0-9]{1,})" class="(.+)" title="(.+)">/';
-//    $replacement = '<a href="$1/index.php?id=$2#c$3" class="$4" title="$5">';
-//    $text = preg_replace($pattern, $replacement, $text);
-    $text = preg_replace_callback($pattern, 'callbackFunction', $text);
-
-    return $text;
-}
-
-function callbackFunction($arg)
-{
-//    return   '<a href="' . $arg[1] . '/index.php?id=' . $arg[2] . '#c' . $arg[3]
-//           . '" class="' . $arg[4] . '" title="' . $arg[5] . '">' . $arg[1] . $arg[3] . $arg[4];
-    return   '<a href="' . $arg[1] . '/index.php?id=' . $arg[2] . '#c' . $arg[3]
-           . '" class="' . $arg[4] . '" title="' . $arg[5] . '">' . showGravatarImage($arg[3], $arg[4]);
-
-}
+$this->plugin->addFilter('recent-comments-text', 'addGravatarImage');
 
 function showGravatarImage($commentID, $class = 'guest')
 {
     global $app;
+
+    // SETTING BEGIN
+    // Image size
+    $size   = 32;
+    // Gravatar rating
+    $rating = 'g';
+    // Images altanative text
+    $alt    = 'His or Her gravatar';
+    // SETTING END
 
     // No object when call from 'recent-comments-text'.
     if (!is_object($app)) { $app = new Loggix_Application; }
 
     // Get E-Mail
     $sql = 'SELECT '
-              .     ' user_mail '
-              . 'FROM ' 
-              .     COMMENT_TABLE . ' '
-              . 'WHERE '
-              .     "id = '" . $commentID . "'";
+         . ' user_mail '
+         . 'FROM ' 
+         . COMMENT_TABLE . ' '
+         . 'WHERE '
+         . "id = '" . $commentID . "'";
 
     $res = $app->db->query($sql);
     $userMail = $res->fetchColumn();
 
+    // Default image
     $defaultUrl = (ereg('admin', $class)) ? $app->getRootUri() . 'theme/css/default/images/icon-admin.png'
                                           : $app->getRootUri() . 'theme/css/default/images/icon-guest.png';
 
-    // Make URL. http://ja.gravatar.com/site/implement/url
+    // Make Gravatar URL
     if ($userMail) {
-        $size    = 32;
-        $rating  = 'g';
-        $alt     = 'His or Her gravatar';
-        $gravatarUrl = 'http://www.gravatar.com/avatar/' . md5(strtolower($userMail))
-                     . '?default='    . urlencode($defaultUrl)
-                     . '&amp;size='   . $size
-                     . '&amp;rating=' . $rating;
-
-        $gravatarImg = '<img src="' . $gravatarUrl . '" width="' . $size .'" height="' . $size .'" alt="' . $alt .'" />';
+        $imageUrl = 'http://www.gravatar.com/avatar/' . md5(strtolower($userMail))
+                  . '?default='    . urlencode($defaultUrl)
+                  . '&amp;size='   . $size
+                  . '&amp;rating=' . $rating;
     } else {
-        $gravatarImg = '<img src="' . $defaultUrl . '" width="' . $size .'" height="' . $size .'" alt="' . $alt .'" />';
+        $imageUrl = defaultUrl;
     }
 
-    return $gravatarImg;
+    return '<img src="' . $imageUrl . '" width="' . $size .'" height="' . $size .'" alt="' . $alt .'" />';
+}
+
+function addGravatarImage($text)
+{
+    // Get ONE recent comment parameters
+    $pattern     = '/<a href="([\.?\.\/]+)index.php\?id=([0-9]{1,})#c([0-9]{1,})" class="(.+)" title="(.+)">/';
+    // Pass parameters to callbackGravatarImage
+    $text = preg_replace_callback($pattern, 'callbackGravatarImage', $text);
+
+    return $text;
+}
+
+function callbackGravatarImage($arg)
+{
+    return '<a href="' . $arg[1] . 'index.php?id=' . $arg[2] . '#c' . $arg[3] .
+           '" class="' . $arg[4] . '" title="' . $arg[5] . '">' . showGravatarImage($arg[3], $arg[4]);
 }
 
